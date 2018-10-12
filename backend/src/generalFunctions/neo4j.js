a4a62c50f2ceb713aea7f68083611b1833b0ff6c
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2018-10-12 04:53:04 -0400

    Update neo4j.js

--- backend/src/generalFunctions/neo4j.js
+++ backend/src/generalFunctions/neo4j.js
@@ -1,7 +1,7 @@
 import { v1 as neo4j } from 'neo4j-driver';
 
 
-const driver = neo4j.driver('bolt://192.168.99.100:7687',neo4j.auth.basic('neo4j','qwerty'));
+const driver = neo4j.driver('neo4j bolt connection');
 
 
 function getNewSession () {
