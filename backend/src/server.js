c93d6684639ed9ccf46b3bfa86780c3d9cc549bf
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2018-10-12 04:50:18 -0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 const PORT = 5000;
 
-mongoose.connect('mongodb://ds217131.mlab.com:17131/gql-test',{user:'yb08',pass:'E@rth2013',useNewUrlParser:true});
+mongoose.connect('mongodb://<connection string>,useNewUrlParser:true});
 mongoose.connection.on('error',function(err) {
           console.log('mongod db down xx '+err);
           process.exit();
